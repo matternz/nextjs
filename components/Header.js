d3d12bed22eb274b406abcb672b0701f6f07b8e6
norrisanator <matthew.norris96@gmail.com>
2019-04-13 00:06:08 +1200

    fixed typo in header component

--- components/Header.js
+++ components/Header.js
@@ -7,10 +7,10 @@ const linkStyle = {
 const Header = () => (
   <div>
     <Link href='/'>
-      <a syle={linkStyle}>Home </a>
+      <a style={linkStyle}>Home </a>
     </Link>
     <Link href='/about'>
-      <a syle={linkStyle}>About </a>
+      <a style={linkStyle}>About </a>
     </Link>
   </div>
 )
